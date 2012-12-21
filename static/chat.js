7a7e648259893189f3fc7556451f54243e123031
Ivan Boldyrev <lispnik@gmail.com>
2012-12-21 11:31:45 +0700

    Begin Backbone migration...

--- static/chat.js
+++ static/chat.js
@@ -7,73 +7,101 @@ function datefmt(ts) {
     return h+':'+m;
 }
 
-function Chat(id) {
-    this.el = $('#'+id);
-}
-
-Chat.prototype.message = function (obj) {
-    var ts = new Date(obj.ts)
-    var msg = $('<div>')
-    if (obj.cmd != 'subject') {
-        msg.append($('<span class="ts">').text('['+datefmt(ts)+'] '));
-    }
-    switch (obj.cmd) {
+var ChatWindow = Backbone.View.extend({
+    el: $('#chat'),
+    message: function (obj) {
+        var ts = new Date(obj.ts)
+        var msg = $('<div>')
+        if (obj.cmd != 'subject') {
+            msg.append($('<span class="ts">').text('['+datefmt(ts)+'] '));
+        }
+        switch (obj.cmd) {
         case 'say':
-        msg.append($('<span class="nick">').text(obj.user+": "),
-                   $('<span class="say">').text(obj.message));
-        break;
+            msg.append($('<span class="nick">').text(obj.user+": "),
+                       $('<span class="say">').text(obj.message));
+            break;
 
         case 'join':
-        msg.append($('<span>').addClass(obj.cmd)
-                   .text(obj.user+" joins."));
+            msg.append($('<span>').addClass(obj.cmd)
+                       .text(obj.user+" joins."));
 
-        $('#roster-list').append($("<li>").text(obj.user));
-        break;
+            $('#roster-list').append($("<li>").text(obj.user));
+            break;
 
         case 'leave':
-        msg.append($('<span>').addClass(obj.cmd)
-                   .text(obj.user+" leaves."));
-        $('#roster-list>li').filter(function (li) {
-            return ($(this).text() == obj.user);
-        }).eq(0).remove();
-        break;
+            msg.append($('<span>').addClass(obj.cmd)
+                       .text(obj.user+" leaves."));
+            $('#roster-list>li').filter(function (li) {
+                return ($(this).text() == obj.user);
+            }).eq(0).remove();
+            break;
 
         case 'ping':
-        // Do nothing, do not even insert message
-        return;
+            // Do nothing, do not even insert message
+            return;
 
         case 'me':
-        msg.append($('<span class="me">').text(obj.user+" "+obj.message));
-        break;
+            msg.append($('<span class="me">').text(obj.user+" "+obj.message));
+            break;
 
         case 'sysmsg':
-        msg.append($('<span class="sysmsg">').text(obj.message));
-        break;
+            msg.append($('<span class="sysmsg">').text(obj.message));
+            break;
 
         case 'subject':
-        msg.append($('<span class="subject">').text("Subject: "+obj.message));
+            msg.append($('<span class="subject">').text("Subject: "+obj.message));
+        }
+        msg.appendTo(this.el);
     }
-    msg.appendTo(this.el);
-};
+});
+
+var LoginWindow = Backbone.View.extend({
+    el: $('#login'),
+    events: {
+        'click #loginbtn': 'login'
+    },
+    
+    login: function (username, group) {
+        var username = $('#username').val().trim();
+        if (username) {
+            $.ajax({ url: '/ajax/login',
+                     data: { 'name': username, group },
+                     dataType: 'json',
+                     type: 'POST',
+                     success: function (data) {
+                         // TODO: dedicated class for roster that handles duplicated
+                         // entries
+                         var rosterList = $('#roster-list');
+                         for (var i in data.users) {
+                             rosterList.append($("<li>").text(data.users[i]));
+                         }
+                     }
+                   });
+            this.$el.hide();
+
+            $("#inputline").attr('disabled', null);
+            $("#sendbtn").attr('disabled', null);
+
+            $("#chat").show();
+            $("#input").show();
+
+            $("#sendbtn").click(function () {
+                say($("#inputline").val(), 'test');
+                $("#inputline").val('').focus();
+            });
+
+            $('#inputline').keyup(function (evt) {
+                if (evt.ctrlKey && (evt.keyCode == 13 || evt.keyCode == 10)) {
+                    $('#sendbtn').click();
+                }
+            });
+        }
+
+    }
+});
 
 
-function login(username, group) {
-    $.ajax({ url: '/ajax/login',
-             data: { 'name': username, 'group': group },
-             dataType: 'json',
-             type: 'POST',
-             success: function (data) {
-                 // TODO: dedicated class for roster that handles duplicated
-                 // entries
-                 var rosterList = $('#roster-list');
-                 for (var i in data.users) {
-                     rosterList.append($("<li>").text(data.users[i]));
-                 }
-             }
-           });
-}
-
-function message(msg, group) {
+function say(msg, group) {
     $.ajax({ url: '/ajax/post',
              data: { 'message': msg, 'group': group },
              type: 'POST'
@@ -83,7 +111,7 @@ function message(msg, group) {
 (function poll(){
     $.ajax({ url: "/ajax/poll", success: function(data){
         for (var i in data) {
-            chat.message(data[i]);
+            chatwin.message(data[i]);
         }
         poll();
     }, dataType: "json",
@@ -97,32 +125,10 @@ function message(msg, group) {
 })();
 
 
-var chat;
+var chatwin;
+var login;
 
 $(document).ready(function () {
-    chat = new Chat("chat");
-
-    $('#loginbtn').click(function () {
-        if ($('#username').val()) {
-            login($('#username').val(), 'test'); // TODO: proper input
-            $('#login').hide();
-
-            $("#inputline").attr('disabled', null);
-            $("#sendbtn").attr('disabled', null);
-
-            $("#chat").show();
-            $("#input").show();
-
-            $("#sendbtn").click(function () {
-                message($("#inputline").val(), 'test'); // TODO proper input
-                $("#inputline").val('').focus();
-            });
-
-            $('#inputline').keyup(function (evt) {
-                if (evt.ctrlKey && (evt.keyCode == 13 || evt.keyCode == 10)) {
-                    $('#sendbtn').click();
-                }
-            });
-        }
-    });
+    chatwin = new ChatWindow();
+    login = new LoginWindow();
 });
